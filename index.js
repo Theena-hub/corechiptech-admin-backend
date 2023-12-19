6d60c54887376db0a1ba7428465e42551feb8675
Theena-hub <theenathayalanramakrishnan@gmail.com>
2023-12-19 18:16:00 +0530

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
-
 app.use('/uploads', express.static('uploads'))
 
 const User = require("./router/user");
@@ -16,7 +15,7 @@ const Product = require("./router/product");
 app.use("/product", Product);
 
 const Banner = require("./router/banner");
-app.use("/banner", Banner);
+app.use("/banner", Banner);    
 
 app.listen(1234,()=>{
     console.log("Server Connected Successfully")
